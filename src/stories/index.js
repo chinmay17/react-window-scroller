1e4b418781fb6b91606d9b8f060bcaca0c67d2ca
chinmay.chaudhary <chinmay.chaudhary@sprinklr.com>
2018-04-13 00:36:03 +0530

    Updates stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { linkTo } from '@storybook/addon-links';
-
-import { Button, Welcome } from '@storybook/react/demo';
 
 import SimpleScrollToTopApp from './ScrollToTop';
 import ScrollToBottomApp from './ScrollToBottom';
@@ -12,11 +9,9 @@ import BothApp from './Both';
 import AnimationScrollToTop from './AnimationScrollToTop';
 import AnimationScrollToBoth from './AnimationScrollToBoth';
 
-storiesOf('Welcome', module).add('to WindowScroller', () => <Welcome showApp={linkTo('Button')} />);
-
 storiesOf('Simple', module)
-  .add('Scroll To Top', () => <SimpleScrollToTopApp scrollBehaviour="instant"/>)
   .add('Scroll To Bottom', () => <ScrollToBottomApp scrollBehaviour="instant"/>)
+  .add('Scroll To Top', () => <SimpleScrollToTopApp scrollBehaviour="instant"/>)
   .add('Both', () => <BothApp scrollBehaviour="instant"/>);
 
 storiesOf('Smooth', module)
